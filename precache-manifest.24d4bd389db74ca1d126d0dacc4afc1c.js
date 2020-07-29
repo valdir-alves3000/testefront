6bdb05f77896554e3a07ebc2961198b09ccb46e0
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2020-07-29 10:29:52 -0300

    Add files via upload

---
+++ precache-manifest.24d4bd389db74ca1d126d0dacc4afc1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b20e9bc6f837c48727dd787b763bb21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a25f95e91a7fd47fe9",
+    "url": "/static/css/main.3e71e1a5.chunk.css"
+  },
+  {
+    "revision": "c045a4707bd566977d30",
+    "url": "/static/js/2.e694c972.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.e694c972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a25f95e91a7fd47fe9",
+    "url": "/static/js/main.7396ad0f.chunk.js"
+  },
+  {
+    "revision": "d0c75788de07ab80db4c",
+    "url": "/static/js/runtime-main.cbe30c6c.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
