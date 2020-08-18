b59a5432a2b52dd76464e0019222d2a7a485d14a
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2020-08-18 13:32:04 -0300

    Add files via upload

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,1 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
 
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  *, button, input {
+    font-family: Roboto, sans-serif;    
+  }
+
+  h1 {
+    font-family: Archivo, sans-serif;
+  }
+
+  :root {
+    --color-primary: #2c2c2c;
+    --color-secondary: #ffb633;
+    --color-tertiary: #f9c450;
+    --color-quartenary: #313131;
+    --color-text: #fff;
+  }
+`;
