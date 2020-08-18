e7d7bbf7b426d2e8abc67f6441f833012a6ee603
valdir-alves3000 <51173956+valdir-alves3000@users.noreply.github.com>
2020-08-18 13:24:31 -0300

    Add files via upload

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,1 +1,28 @@
 
+export default [
+
+   {
+      empresa: 'Compass Minerals',
+      cargo: 'Op. Empilhadeira',
+      periodo: '07/2019 - 10/2019'
+   },
+
+   {
+      empresa: 'CLO Usinagem',
+      cargo: 'Op. Empilhadeira',
+      periodo: '06/2018 - 03/2019'
+   },
+
+   {
+      empresa: 'Célere Logística',
+      cargo: 'Op. Empilhadeira',
+      periodo: '05/2011 - 11/2017'
+   },
+
+   {
+      empresa: 'Produquímica',
+      cargo: 'Op. Empilhadeira',
+      periodo: '06/2010 - 12/2010'
+   },
+
+];
